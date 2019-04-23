e1fe0b5fc3d06acd675c544bf54f7eb55b842cfc
Aki Imai <akimai808@gmail.com>
2019-04-23 11:33:05 -0700

    chu add

--- src/component/ChuDaiko.js
+++ src/component/ChuDaiko.js
@@ -9,9 +9,8 @@ class ChuDaiko extends Component {
 
     render() {
         return (
-            <div className="chu-ctn">
-                {/* <div><span className="add-chu">+</span></div> */}
-                {/* <img id="chu-daiko" onClick={this.handleClick} src="https://upload.wikimedia.org/wikipedia/commons/1/16/Miya_Daiko_drum_-_Taiko_drums.jpg" alt="chu-daiko"></img> */}
+            <div className="chu-test">
+                <img id="chu-daiko" onClick={this.handleClick} src="https://upload.wikimedia.org/wikipedia/commons/1/16/Miya_Daiko_drum_-_Taiko_drums.jpg" alt="chu-daiko"></img>
             </div>
         )
     }
--- src/component/Main.js
+++ src/component/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Settings from './Settings';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
+import ChuDaiko from './ChuDaiko';
 
 class Main extends Component {
     constructor(props) {
@@ -11,7 +12,8 @@ class Main extends Component {
             width: 700,
             height: 400,
             color: '#000',
-            weight: 3
+            weight: 3,
+            chu: []
         }
     }
 
@@ -39,10 +41,25 @@ class Main extends Component {
         })
     }
 
+    addChu = () => {
+        let chu = [...this.state.chu]
+        chu.push(<ChuDaiko />)
+        this.setState({
+            chu
+        })
+    }
+
     render() {
+        let chu = this.state.chu.map((item, index) => {
+            return <div className="chu-item" key={index}>{item}</div>
+        });
+
         return (
             <div>
                 <div className="side-bar">
+                    <div>
+                        <span><i className='fas fa-times'></i></span>
+                    </div>
                     <ul>
                         <li>
                             <label htmlFor="color">Color:</label>
@@ -53,12 +70,18 @@ class Main extends Component {
                             <input type="number" id="weight" min="2" max="200" value={this.state.weight} onChange={this.handleWeight} />
                         </li>
                         <li>
-                            <button id="clear"><i class='fas fa-trash-alt'></i></button>
+                            <button id="clear"><i className='fas fa-trash-alt'></i></button>
                         </li>
                     </ul>
+
+                    <button onClick={this.addChu}>Chu Add</button>
                 </div>
                 
                 <Settings selectHeight={this.handleHeight} selectWidth={this.handleWidth} />
+
+                <div id="chu-ctn">
+                    {chu}
+                </div>
 
                 <P5Wrapper sketch={sketch} height={this.state.height} width={this.state.width} color={this.state.color} weight={this.state.weight}></P5Wrapper>
 
